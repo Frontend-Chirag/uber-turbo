2d4410426f6df210e6fa293b818d81a65029fd28
anujkashyap123000 <anujkashyap123000@gmail.com>
2025-07-22 08:39:43 +0000

    update docker

--- apps/uber/next.config.ts
+++ apps/uber/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../")
 };
+
 
 export default nextConfig;
